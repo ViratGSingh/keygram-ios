d2e6a19bdf7fca87cd8407632a947c39da0e3edf
Virat G Singh <viratgsingh99@gmail.com>
2026-05-03 12:53:02 +0530

    Initial Commit

---
+++ keygram/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  keygram
+//
+//  Created by Virat Singh on 03/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ keygram/keygramApp.swift
@@ -0,0 +1,17 @@
+//
+//  keygramApp.swift
+//  keygram
+//
+//  Created by Virat Singh on 03/05/26.
+//
+
+import SwiftUI
+
+@main
+struct keygramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
